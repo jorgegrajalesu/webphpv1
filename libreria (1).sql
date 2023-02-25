2b71e0027cc279e2f573c021f7a568dfb3f8129a
jorgegrajalesu <jegosystem@gmail.com>
2023-02-25 15:55:34 -0500

    vbd250223

--- libreria (1).sql
+++ libreria (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2023 a las 17:06:27
+-- Tiempo de generación: 25-02-2023 a las 21:54:41
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.15
 
@@ -52,6 +52,13 @@ CREATE TABLE `libros` (
   `descripcion` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`idlibro`, `nombre`, `isbn`, `precio`, `imagen`, `descripcion`) VALUES
+(1, 'Crimen y castigo', '955568788', 95000, 'images/crimenycastigo.jpg', 'Autor: Fiódor Dostoievski\r\nEditorial: Edimat\r\nTamaño: 20 x 13 cm\r\nNº de páginas: 400\r\nEstado: Nuevo\r\nTapa DURA');
+
 -- --------------------------------------------------------
 
 --
@@ -62,8 +69,15 @@ CREATE TABLE `usuarios` (
   `idusuario` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `clave` varchar(15) NOT NULL
+  `clave` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `nombre`, `email`, `clave`) VALUES
+(1, 'jorge', 'aprendesoftware@poliandino.edu.co', '35bec5abade80678f8dc02a1cabffe92');
 
 --
 -- Índices para tablas volcadas
@@ -101,13 +115,13 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `idlibro` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idlibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
