e1c074af072540e1b8d0e0a01c9a074014b811fa
jorgegrajalesu <jegosystem@gmail.com>
2023-03-18 15:21:28 -0500

    v3

--- admin/index.php
+++ admin/index.php
@@ -1,117 +1,98 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web libreria</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="admin/dist/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="images/logorestaurantetapitas.ico" type="image/x-icon">   
-    
-    
+    <link rel="shortcut icon" href="images/logorestaurantetapitas.ico" type="image/x-icon">
+
+
 </head>
+
 <body>
-    <!-- menú -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark menu">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.php">
-            <img src="admin/images/logo.jpg" alt="logo " class="logo"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto me-2">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="consultalibros.php">Libros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#servicios">Servicios</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contacto">Contacto</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link btn btn-danger" href="https://wa.me/3105188168?text=Solicita%20tu%20pedido!!" tabindex="-1" target="_blank">¡Haz tu pedido!</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <?php
+    //verificar si existe el usuario
+    if (isset($_REQUEST['login'])) {
+      session_start();
+      $email = $_REQUEST['email'] ?? '';
+      $clave = $_REQUEST['clave'] ?? '';
+      $clave =md5($clave);
+      
+      //consultar
+      include_once "conexion.php";
+      $conn = mysqli_connect($host, $user, $pw, $db);
+      $sql = "SELECT * FROM usuarios WHERE email ='".$email."' and clave = '".$clave."'; ";
+      
+      //resultado de la peticion de consulta
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_array($result);
+      
+      //si el usuario es correcto, va ir a la pagina del panel adaministrador
+      if($row){
+        $_SESSION['idusuario'] = $row['idusuario'];
+        $_SESSION['nombre'] = $row['nombre'];
+        $_SESSION['email'] = $row['email'];
+        
+        header("Location:panel.php");
+      }else{
+        ?>
+    <div class="alert alert-danger" role="alert">
+        <strong>Error en login <img src="images/Error.svg.png" width="50" alt=""></strong>
 
-
-   
-      <!-- servicios -->
-      <div class="row row-cols-1 row-cols-md-2 g-4 p-5" id="servicios">
+    </div>
+    <?php
+       }
+      }
+    ?>
+    <!-- iniciar sesión -->
+    <div class=" row row-cols-1 row-cols-md-1 g-4 p-5" id="login">
         <h2 class="tituloSer">Login</h2>
-        <form class="row g-3">
-            <div class="col-md-6">
+        <form class="row g-3" method="post">
+            <div class=" col-md-6">
                 <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
+                <input type="email" class="form-control" name="email" id="inputEmail4">
             </div>
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <label for="inputPassword4" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
+                <input type="password" class="form-control" name="clave" id=" inputPassword4">
             </div>
+
             <div class="col-12">
-                <label for="inputAddress" class="form-label">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                <button type="submit" class="btn btn-primary" name="login">Ingresar</button>
             </div>
-            <div class="col-12">
-                <label for="inputAddress2" class="form-label">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-            </div>
-            <div class="col-md-6">
-                <label for="inputCity" class="form-label">City</label>
-                <input type="text" class="form-control" id="inputCity">
-            </div>
-            <div class="col-md-4">
-                <label for="inputState" class="form-label">State</label>
-                <select id="inputState" class="form-select">
-                <option selected>Choose...</option>
-                <option>...</option>
-                </select>
-            </div>
-            <div class="col-md-2">
-                <label for="inputZip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
-            </div>
-            <div class="col-12">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
-                    Check me out
-                </label>
-                </div>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
-            </form>                      
-      </div>
-
-    
-
-     
-
-
-      <!-- footer -->
-      <footer class="cover">
-        &copy;2023. Todos los derechos reservados Restaurante Tapitas. Creado por:<a href="#" target="_blank">Jorge Grajales</a>
-      </footer>
+        </form>
+    </div>
 
 
 
-    
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <!-- footer -->
+    <footer class=" cover">
+        &copy;2023. Todos los derechos reservados Restaurante Tapitas. Creado por:<a href="#" target="_blank">Jorge
+            Grajales</a>
+    </footer>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
